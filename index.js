be3845e6eb647c12a447bc9e864db0e4412abd7b
Klove Adams <kloveadams@gmail.com>
2021-09-28 16:39:09 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const server = require('./api/server');
 
-console.log("Inside index.js");
-
 const port = 5000;
 
-// START YOUR SERVER HERE
 server.listen(port, () => {
-    console.log(`listening on port ${port}`)
-  })
+  console.log(`listening on port ${port}`)
+})
